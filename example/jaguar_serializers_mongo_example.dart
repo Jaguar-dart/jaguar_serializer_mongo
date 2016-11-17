1bdff27013e4e1d21d6978222d12547702469a72
Ravi Teja Gudapati <tejainece@gmail.com>
2016-11-17 18:10:41 +0100

    Removed unnecessary files

--- example/jaguar_serializers_mongo_example.dart
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2016, Ravi Teja Gudapati. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-main() {
-
-}
